630298044296f7a947937b7e4100fae6a9b56a81
Josh Benloulou <jbenloul@gmail.com>
2017-05-07 15:43:06 -0700

    functioning version

--- keys.js
+++ keys.js
@@ -1,9 +1,8 @@
-JavaScript
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<vk4XCOWA3bokDynfSs8NRXsB9>',
-  consumer_secret: '<yA1mRDzu7NfxrvtIWaYxFPyBAcce2Ow5nzeEpgcloaOnwVct4y>',
-  access_token_key: '<25885457-Ax9p6kBs6dr2DIsHKHJHIEZ0juy3MV6KYhFtAPVfS>',
-  access_token_secret: '<gJn2YE37m18kAYe4iQvrk91o3xk1JWz99MN7mIzpICfdc>',
+  consumer_key: 'vk4XCOWA3bokDynfSs8NRXsB9',
+  consumer_secret: 'yA1mRDzu7NfxrvtIWaYxFPyBAcce2Ow5nzeEpgcloaOnwVct4y',
+  access_token_key: '25885457-Ax9p6kBs6dr2DIsHKHJHIEZ0juy3MV6KYhFtAPVfS',
+  access_token_secret: 'gJn2YE37m18kAYe4iQvrk91o3xk1JWz99MN7mIzpICfdc',
 }
--- liri.js
+++ liri.js
@@ -1,89 +1,151 @@
-// ### What Each Command Should Do
+var request = require("request");
+var fs = require('fs');
 
-// 1. `node liri.js my-tweets`
-
-//    * This will show your last 20 tweets and when they were created at in your terminal/bash window.
-
-// 2. `node liri.js spotify-this-song '<song name here>'`
-
-//    * This will show the following information about the song in your terminal/bash window
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
-
-//    * if no song is provided then your program will default to
-//      * "The Sign" by Ace of Base
-
-// 3. `node liri.js movie-this '<movie name here>'`
-
-//    * This will output the following information to your terminal/bash window:
-
-//      ```
-//        * Title of the movie.
-//        * Year the movie came out.
-//        * IMDB Rating of the movie.
-//        * Country where the movie was produced.
-//        * Language of the movie.
-//        * Plot of the movie.
-//        * Actors in the movie.
-//        * Rotten Tomatoes URL.
-//      ```
-
-//    * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-//      * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-//      * It's on Netflix!
-
-// 4. `node liri.js do-what-it-says`
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-//      * Feel free to change the text in that document to test out the feature for other commands.
-
-
-"my-tweets"
-"spotify-this-song"
-"movie-this"
-"do-what-it-says"
-
-// Everything above is what is given to us already______________________________________________________________
-
-var command = process.argv[2]
-
-
+var Twitter = require('twitter');
+var twitterKeys = require('./keys.js').twitterKeys
+var tweet = ""
+var movieName = ""
+var client = new Twitter(twitterKeys)
+var command = process.argv[2];
+var queryUrl;
 
 if (command === "my-tweets") {
-    //* This will show your last 20 tweets and when they were created at in your terminal/bash window.
-    //https://www.npmjs.com/package/node-tweet-stream
+    console.log("Tweets will show here");
+
+    if (process.argv.length > 3) {
+        console.log('this worked')
+
+        for (var i = 3; i < process.argv.length; i++) {
+            tweet += process.argv[i] += "+"
+            var tweetSearch = tweet.slice(0, -1);
+        }
+        console.log(tweetSearch);
+
+        client.get('search/tweets', { q: tweetSearch, result_type: "recent", count: 20 }, function(error, tweets, response) {
+            // console.log(response);
+            for (var i = 0; i < tweets.statuses.length; i++) {
+                tweets[i]
+                console.log(tweets.statuses[i].text);
+            }
+            console.log(tweets.statuses[0].text);
+        });
+    }
 } else if (command === "spotify-this-song") {
-	//https://www.npmjs.com/package/spotify-playlist-downloader
- //    * This will show the following information about the song in your terminal/bash window
-//      * Artist(s)
-//      * The song's name
-//      * A preview link of the song from Spotify
-//      * The album that the song is from
+    var songName = '';
+    var songNameFinal;
 
-//    * if no song is provided then your program will default to
-//      * "The Sign" by Ace of Base
+    if (process.argv.length > 3) {
+        console.log('this worked')
+
+        for (var i = 3; i < process.argv.length; i++) {
+            songName += process.argv[i] += "+"
+            songNameFinal = songName.slice(0, -1);
+        }
+        console.log(songNameFinal);
+
+        queryUrl = "https://api.spotify.com/v1/search?q=" + songNameFinal + "&type=track"
+        console.log(queryUrl);
+
+        request(queryUrl, function(error, response, body) {
+            console.log("The song's Artist is: " + JSON.parse(body).tracks.items[0].album.artists[0].name);
+            console.log("Even though you searched for it specifically, we're asked to provide you the song's name, so here goes: " + JSON.parse(body).tracks.items[0].name);
+            console.log("The song's preview link: " + JSON.parse(body).tracks.items[0].external_urls.spotify);
+            console.log("The song is from the Album: " + JSON.parse(body).tracks.items[0].album.name);
+        });
+    }
 } else if (command === "movie-this") {
+    if (process.argv.length > 3) {
+        console.log('this worked')
 
-//    * This will output the following information to your terminal/bash window:
+        for (var i = 3; i < process.argv.length; i++) {
+            movieName += process.argv[i] += "+"
+            movieInputFinal = movieName.slice(0, -1);
+        }
+        console.log(movieInputFinal);
 
-//      ```
-//		console log the path for all of these 
-//        * Title of the movie.
-//        * Year the movie came out.
-//        * IMDB Rating of the movie.
-//        * Country where the movie was produced.
-//        * Language of the movie.
-//        * Plot of the movie.
-//        * Actors in the movie.
-//        * Rotten Tomatoes URL.
-// * If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-//      * If you haven't watched "Mr. Nobody," then you should: <http://www.imdb.com/title/tt0485947/>
-//      * It's on Netflix!
-    
+        queryUrl = "http://www.omdbapi.com/?t=" + movieInputFinal + "&y=&plot=short&r=json"
+        console.log(queryUrl)
+            //    * This will output the following information to your terminal/bash window:
+        request(queryUrl, function(error, response, body) {
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+                console.log("The movie's Title is: " + JSON.parse(body).Title);
+                console.log("The movie's release year is: " + JSON.parse(body).Year);
+                // console.log("The movie's IMDB Rating is :" + JSON.parse(body).Ratings[0].Value);
+                console.log("The movie's IMDB Rating is: " + JSON.parse(body).imdbRating);
+                console.log("The movie's runtime is: " + JSON.parse(body).Runtime);
+                console.log("The movie's Country of production is: " + JSON.parse(body).Country);
+                console.log("The movie's Language(s) is/are: " + JSON.parse(body).Language);
+                console.log("The movie's Plot is: " + JSON.parse(body).Plot);
+                console.log("The movie's Actors are: " + JSON.parse(body).Actors);
+                console.log("The movie's URL is: " + JSON.parse(body).Website);
+            }
+        })
+    } else if (process.argv.length === 3) {
+        queryUrl = "http://www.omdbapi.com/?t=Mr+Nobody&y=&plot=short&r=json"
+            //    * This will output the following information to your terminal/bash window:
+        request(queryUrl, function(error, response, body) {
+            // If the request is successful
+            if (!error && response.statusCode === 200) {
+                console.log("The movie's Title is: " + JSON.parse(body).Title);
+                console.log("The movie's release year is: " + JSON.parse(body).Year);
+                // console.log("The movie's IMDB Rating is :" + JSON.parse(body).Ratings[0].Value);
+                console.log("The movie's IMDB Rating is: " + JSON.parse(body).imdbRating);
+                console.log("The movie's runtime is: " + JSON.parse(body).Runtime);
+                console.log("The movie's Country of production is: " + JSON.parse(body).Country);
+                console.log("The movie's Language(s) is/are: " + JSON.parse(body).Language);
+                console.log("The movie's Plot is: " + JSON.parse(body).Plot);
+                console.log("The movie's Actors are: " + JSON.parse(body).Actors);
+                console.log("The movie's URL is: " + JSON.parse(body).Website);
+            }
+        })
+    }
 } else if (command === "do-what-it-says") {
-//    * Using the `fs` Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-//      * It should run `spotify-this-song` for "I Want it That Way," as follows the text in `random.txt`.
-//      * Feel free to change the text in that document to test out the feature for other commands.
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        //console.log(data);
+        var dataArray = data.split(",");
+        for (var i = 0; i < dataArray.length; i += 2) {
+            command = dataArray[i].trim();
+            var textFileInput = dataArray[i + 1].trim();
+            console.log(command);
+            // console.log(textFileInput)
+            if (command === "spotify-this-song") {
+                queryUrl = "https://api.spotify.com/v1/search?q=" + textFileInput + "&type=track";
+                request(queryUrl, function(error, response, body) {
+                    console.log("The song's Artist is: " + JSON.parse(body).tracks.items[0].album.artists[0].name);
+                    console.log("Even though you searched for it specifically, we're asked to provide you the song's name, so here goes: " + JSON.parse(body).tracks.items[0].name);
+                    console.log("The song's preview link: " + JSON.parse(body).tracks.items[0].external_urls.spotify);
+                    console.log("The song is from the Album: " + JSON.parse(body).tracks.items[0].album.name);
+                });
+            } else if (command === "my-tweets") {
+                client.get('search/tweets', { q: textFileInput, result_type: "recent", count: 20 }, function(error, tweets, response) {
+                    // console.log(response);
+                    for (var i = 0; i < tweets.statuses.length; i++) {
+                        tweets[i]
+                        console.log(tweets.statuses[i].text);
+                    }
+                })
+            } else if (command === "movie-this") {
+                queryUrl = "http://www.omdbapi.com/?t=" + textFileInput + "&y=&plot=short&r=json"
+                console.log(queryUrl)
+                    //    * This will output the following information to your terminal/bash window:
+                request(queryUrl, function(error, response, body) {
+                    // If the request is successful
+                    if (!error && response.statusCode === 200) {
+                        console.log("The movie's Title is: " + JSON.parse(body).Title);
+                        console.log("The movie's release year is: " + JSON.parse(body).Year);
+                        // console.log("The movie's IMDB Rating is :" + JSON.parse(body).Ratings[0].Value);
+                        console.log("The movie's IMDB Rating is: " + JSON.parse(body).imdbRating);
+                        console.log("The movie's runtime is: " + JSON.parse(body).Runtime);
+                        console.log("The movie's Country of production is: " + JSON.parse(body).Country);
+                        console.log("The movie's Language(s) is/are: " + JSON.parse(body).Language);
+                        console.log("The movie's Plot is: " + JSON.parse(body).Plot);
+                        console.log("The movie's Actors are: " + JSON.parse(body).Actors);
+                        console.log("The movie's URL is: " + JSON.parse(body).Website);
+                    }
+                })
+
+            }
+        }
+    });
 }
